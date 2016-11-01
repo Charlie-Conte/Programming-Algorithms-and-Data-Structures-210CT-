7e7ffdda26ace8f4aea03a9380af8a6e26f17a19
Charlie Conte <cementor@hotmail.co.uk>
2016-11-01 17:20:25 +0000

    Task1 part 1 no description yet

--- Programming-Algorithms-and-Data-Structures-210CT-/src/week1/task1.java
+++ Programming-Algorithms-and-Data-Structures-210CT-/src/week1/task1.java
@@ -1,11 +1,43 @@
 package week1;
 
-public class task1 
-{
+import main.Manager.Tasks;
+import java.util.Random;
 
-	public static void shuffle() 
+
+public class task1 implements Tasks
+{
+	@Override
+
+	public void run()
 	{
 		System.out.println("Success");
+		main.Manager.consoleSpace(1);
+		
+		
+		int[] inputArray = new int[]{5,3,8,6,1,9,2,7};
+				
+		
+		randomizeArray(inputArray);		
+		
+		System.out.println("int array");
+		for (int i=0; i<inputArray.length; i++)
+		{
+			System.out.print(inputArray[i]+" ");
+		}
+		
 	}
-	
+	public static int[] randomizeArray(int[] array)
+	{
+		Random rgn = new Random();  		
+ 
+		for (int i=0; i<array.length; i++) 
+		{
+		    int randomPosition = rgn.nextInt(array.length);
+		    int temp = array[i];
+		    array[i] = array[randomPosition];
+		    array[randomPosition] = temp;
+		}
+ 
+		return array;
+	}
 }
